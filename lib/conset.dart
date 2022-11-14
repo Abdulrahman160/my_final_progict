4b4e59f03bf86a46fcb19de8c689c85de40fce0d
Abdulrahman160 <112969097+Abdulrahman160@users.noreply.github.com>
2022-11-14 23:19:33 +0200

    hallo

--- lib/conset.dart
+++ lib/conset.dart
@@ -2,3 +2,8 @@
 import 'dart:ui';
 
 Color backGroundColor=Color(0XFFF6F6F6);
+Color backGroundColo=Color(0XFFF6F6F6);
+Color backGroundCor=Color(0XFFF6F6F6);
+Color backGrondColor=Color(0XFFF6F6F6);
+Color backGroundlor=Color(0XFFF6F6F6);
+Color backGrouColor=Color(0XFFF6F6F6);
