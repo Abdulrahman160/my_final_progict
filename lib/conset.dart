0d20161a44356a518933e7d6490da19794971c53
AbdoAshoor2001 <113042441+AbdoAshoor2001@users.noreply.github.com>
2022-11-15 15:01:46 +0200

    بسم الله

--- lib/conset.dart
+++ lib/conset.dart
@@ -6,6 +6,7 @@ class StartView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      // بسم الله الرحمن الرحيم
       color: Colors.black,
     );
   }
