fb59bc72babdaa6b61631fd317d7447da6401651
AbdoAshoor2001 <113042441+AbdoAshoor2001@users.noreply.github.com>
2022-11-17 02:03:47 +0200

    error

--- lib/view/Error.dart
+++ lib/view/Error.dart
@@ -6,7 +6,31 @@ class ErrorView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Error',style: TextStyle(color: Color(0xff2E2E5D),fontSize: 16,fontWeight: FontWeight.w500),),
+        shadowColor: Color(0xffF3F1FE).withOpacity(0.2),
+        backgroundColor: Color(0xffFFFFFF).withOpacity(0.8),
+        actions: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            // mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Container(
+                padding: EdgeInsets.only(left: 8),
+                alignment: Alignment.center,
+                height: 48,
+                width: 48,
+                decoration: BoxDecoration(
+                    color: Color(0xffF2F2F5).withOpacity(0.4),
+                    borderRadius: BorderRadius.circular(40)
+                ),
+                child: Icon(Icons.arrow_back_ios,color: Colors.black,),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/widget/Container_Button_nonColor.dart
+++ lib/widget/Container_Button_nonColor.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import '../conset.dart';
 
 class ContainernonColorView extends StatelessWidget {
